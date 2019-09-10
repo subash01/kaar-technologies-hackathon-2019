c96cdc0d1ad2f81739a3ea3489e928c509a1ca30
subash01 <subashr9803@gmail.com>
2019-09-10 21:39:25 +0530

    Add files via upload

---
+++ headres.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+<title>Metro Water Forum</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<header>
+<div class="main">
+<div class="logo">
+
+</div>
+<ul>
+<li class="active"><a href="#">Home</a></li>
+<li><a href="http://localhost/hackathon/admin/">Admin Login</a></li>
+<li><a href="http://localhost/hackathon/users/">Login</a></li>
+<li><a href="http://localhost/hackathon/users/registration.php">Register</a></li>
+<li><a href="#">Contacts</a></li>
+</ul>
+</div>
+<div class="title">
+<h1>Metro Water Services</h1>
+</div>
+
+</header>
+
+</body>
+
+</html>
